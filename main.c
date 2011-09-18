30d740489441e1cdeb7615999e353e13bec54692
luckyxxl <lukas@ulti.tech>
2011-09-18 14:21:23 +0000

    Benutzt nun Unterordner für Shader/Texturen

--- main.c
+++ main.c
@@ -121,12 +121,12 @@ int main()
 	
 	drawer_init();
 	
-	wall_texture = drawer_load_texture("wall.jpg");
-	ceiling_texture = drawer_load_texture("ceiling.jpg");
-	floor_texture = drawer_load_texture("floor.jpg");
+	wall_texture = drawer_load_texture("Textures/wall.jpg");
+	ceiling_texture = drawer_load_texture("Textures/ceiling.jpg");
+	floor_texture = drawer_load_texture("Textures/floor.jpg");
 	
-	textured_program = drawer_create_program("textured.glslv", "textured.glslf");
-	twister_program = drawer_create_program("twister.glslv", "twister.glslf");
+	textured_program = drawer_create_program("Shader/textured.glslv", "Shader/textured.glslf");
+	twister_program = drawer_create_program("Shader/twister.glslv", "Shader/twister.glslf");
 	
 	pyramid = mesh_create_pyramid(0.2);
 	
