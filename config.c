ce385fbde3a61cc846dbf67e6a909d89fb12a461
luckyxxl <lukas@ulti.tech>
2011-11-17 11:47:20 +0000

    Speichern der Konfiguration geändert

--- config.c
+++ config.c
@@ -4,56 +4,47 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Configuration
+struct ConfigEntry
 {
 	char *name, *value;
+	struct ConfigEntry *next;
 };
 
-struct Configuration *configurations;
-int configurations_count;
+struct ConfigEntry *config_first = NULL;
 
 static char* string_create_from(char *other, int count);
 static int string_get_char_index(char *str, char c);
 
 void config_load()
 {
-	char str[128];
-	
 	FILE *file = fopen("config.txt", "r");
-	
-	configurations_count = 0;
-	while(fgets(str, 128, file) != NULL) configurations_count++;
-	rewind(file);
-	
-	configurations = malloc(sizeof(struct Configuration) * configurations_count);
-	int i = 0;
+	struct ConfigEntry *last = NULL;
+	char str[128];
 	while(fgets(str, 128, file) != NULL)
 	{
-		struct Configuration *config = &configurations[i++];
+		struct ConfigEntry *new_entry = malloc(sizeof(struct ConfigEntry));
 		int symbol_index = string_get_char_index(str, '=');
-		config->name = string_create_from(str, symbol_index);
-		config->value = string_create_from(str+symbol_index+1, strlen(str)-symbol_index-2);
+		new_entry->name = string_create_from(str, symbol_index);
+		new_entry->value = string_create_from(str+symbol_index+1, strlen(str)-symbol_index-2);
+		new_entry->next = NULL;
+		if(!config_first) config_first = new_entry;
+		if(last) last->next = new_entry;
+		last = new_entry;
 	}
 }
 
 void config_print()
 {
-	int i;
-	for(i=0; i<configurations_count; i++)
-	{
-		struct Configuration *config = &configurations[i];
-		printf("%d: %s=%s\n", i, config->name, config->value);
-	}
+	struct ConfigEntry *config;
+	for(config = config_first; config != NULL; config = config->next)
+		printf("%s=%s\n", config->name, config->value);
 }
 
 const char* config_get_value(char *name)
 {
-	int i;
-	for(i=0; i<configurations_count; i++)
-	{
-		struct Configuration *config = &configurations[i];
+	struct ConfigEntry *config;
+	for(config = config_first; config != NULL; config = config->next)
 		if(strcmp(config->name, name) == 0) return config->value;
-	}
 	return NULL;
 }
 
