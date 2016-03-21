7defdb4962d05ecd6c28be136bea52c644ede7fa
David Greiner <david@ulti.tech>
2016-03-21 14:15:15 +0100

    VS: Implement missing usleep function

--- src/main.c
+++ src/main.c
@@ -22,6 +22,24 @@
 #include "CoreFoundation/CoreFoundation.h"
 #endif
 
+#ifdef _WIN32
+#include <windows.h>
+
+void usleep(__int64 usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER ft;
+
+	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
+
+#endif
+
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
