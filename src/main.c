a3c13cb2d7a39c90601ec8994de65e597c4a571f
luckyxxl <mail@luckyxxl.de>
2016-10-25 08:07:48 +0200

    restructured main

--- src/main.c
+++ src/main.c
@@ -21,6 +21,63 @@
 #include "CoreFoundation/CoreFoundation.h"
 #endif
 
+#if defined SCREENSAVER
+enum screensaverParameter { NONE, CONFIGURATION, PREVIEW, FULLSCREEN };
+
+static enum screensaverParameter get_screensaver_parameter(int argc, const char *argv[])
+{
+	if(argc == 1) return CONFIGURATION;
+	else if(argc == 2)
+	{
+		char* arg = argv[1];
+		if(arg[0] == '-' || arg[0] == '/')
+		{
+			switch(tolower(arg[1]))
+			{
+				case 'c':
+					return CONFIGURATION;
+				case 'p':
+					return PREVIEW; // Preview in Settings screen
+				case 's':
+					return FULLSCREEN; // When user clicks Preview button in Settings screen
+			}
+		}
+	}
+
+	return NONE;
+}
+#endif
+
+static void run()
+{
+	config_load();
+	config_print();
+
+	window_init();
+	drawer_init();
+	scene_init();
+
+	float time_passed = 0.0;
+
+	while(window_do_events())
+	{
+		int start = SDL_GetTicks();
+
+		scene_update(time_passed);
+
+		drawer_begin_scene(time_passed);
+		scene_draw();
+		drawer_end_scene();
+
+		int end = SDL_GetTicks();
+		time_passed = (end-start)/1000.0;
+	}
+
+	scene_quit();
+	drawer_quit();
+	window_quit();
+}
+
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
@@ -53,75 +110,30 @@ int main(int argc, char *argv[])
 	}
 #endif
 
-#if defined SCREENSAVER
-	enum screensaverParameter { NONE, CONFIGURATION, PREVIEW, FULLSCREEN };
-	enum screensaverParameter parameter = NONE;
-	if(argc > 1)
-	{
-		char* cmdLine = argv[1];
-		if(cmdLine[0] == '-' || cmdLine[0] == '/')
-		{
-			switch(tolower(cmdLine[1]))
-			{
-			case 'c':
-				parameter = CONFIGURATION;
-				break;
-			case 'p':
-				parameter = PREVIEW; // Preview in Settings screen
-				break;
-			case 's':
-				parameter = FULLSCREEN; // When user clicks Preview button in Settings screen
-				break;
-			default:
-				parameter = NONE;
-			}
-		}
-	}
-
-	if(argc == 1 || parameter == CONFIGURATION)
-	{
-		system("notepad config.txt");
-	}
-	else if(parameter == PREVIEW)
-	{
-		//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "No preview", "This screensaver can not be previewed.", NULL);
-	}
-	else if(parameter == FULLSCREEN)
-	{
-#endif
-
+	//TODO: instead of chdir, pass the path here
 	file_set_resource_dir("./");
+
 	file_set_output_dir("./");
 
-	config_load();
-	config_print();
-
-	window_init();
-	drawer_init();
-	scene_init();
-
-	float time_passed = 0.0;
-
-	while(window_do_events())
-	{
-		int start = SDL_GetTicks();
-
-		scene_update(time_passed);
-
-		drawer_begin_scene(time_passed);
-		scene_draw();
-		drawer_end_scene();
-
-		int end = SDL_GetTicks();
-		time_passed = (end-start)/1000.0;
-	}
-
-	scene_quit();
-	drawer_quit();
-	window_quit();
-
 #if defined SCREENSAVER
+	enum screensaverParameter parameter = get_screensaver_parameter(argc, argv);
+
+	switch(parameter)
+	{
+		case NONE:
+			break;
+		case CONFIGURATION:
+			system("notepad config.txt");
+			break;
+		case PREVIEW:
+			//SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "No preview", "This screensaver can not be previewed.", NULL);
+			break;
+		case FULLSCREEN:
+			run();
+			break;
 	}
+#else
+	run();
 #endif
 
 	return 0;
